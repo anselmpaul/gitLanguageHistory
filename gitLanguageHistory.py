61231cbf1e18ea25c3bf95fa3b42506c9e1ce34c
anselm <anselm.bunsen@sandstorm.de>
2021-01-14 10:34:16 +0100

    added option to run with path, updated csv parsing

--- gitLanguageHistory.py
+++ gitLanguageHistory.py
@@ -4,60 +4,50 @@ import subprocess
 import datetime
 import csv
 import math
+import argparse
 
 def main():
     begin_time = datetime.datetime.now()
-    repo = git.Repo('.')
+    parser = argparse.ArgumentParser(description='Create a history of the programming languages used in your git repo.')
+    parser.add_argument('--path', nargs='?', default='.',
+                        help='path to your git repo, defaults to the current directory')
+
+    args = parser.parse_args()
+
+    repo = git.Repo(args.path)
     commitCount = len(list(repo.iter_commits()))
-    commits = []
     branch = repo.active_branch
-    allLanguages = []
+    writtenLines = 0
 
     print('analysing', commitCount, 'commits. This is going to take some time...')
+
+    result = csv.writer(open('gitLanguageHistory.csv', 'w'), delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    headers = ['commitSha', 'commitMessage', 'commitDate', 'language', 'percent']# + allLanguages
+    result.writerow(headers)
     
     for index, commit in enumerate(list(repo.iter_commits()), start=1):        
         # need to filter out semis and linebreaks
         commitMsg = commit.message.rstrip().replace(';', ',').replace('\n', ',')
         if "Merge branch" not in commitMsg:
-            commitData = {
-                'commitSha': commit.hexsha,
-                'commitMessage': commitMsg,
-                'commitDate': commit.committed_datetime.strftime("%d-%m-%Y")
-            }
-
             repo.git.checkout(commit.hexsha)
-            languages = {}
-            linguistResult = subprocess.check_output('github-linguist').decode("utf-8").split('\n')
+            linguistResult = subprocess.check_output('github-linguist', cwd=args.path.encode('utf-8')).decode("utf-8").split('\n')
             for language in linguistResult:
+                row = [commit.hexsha, commitMsg, commit.committed_datetime.strftime("%d.%m.%Y"), "", ""]
                 percentAndLanguage = language.split()
                 if len(percentAndLanguage) > 1:
-                    languages[percentAndLanguage[1]] = percentAndLanguage[0].replace('%', '')
-            commitData = commitData | languages
-            usedLanguages = languages.keys()
-            allLanguages = list(set(allLanguages) | set(usedLanguages))
-            commits.append(commitData)
+                    row[3] = percentAndLanguage[1]
+                    row[4] = percentAndLanguage[0].replace('%', '')
+                    result.writerow(row)
+                    writtenLines = writtenLines + 1
 
         if (index > 0 and index % 10 == 0):
             timePassed = datetime.datetime.now() - begin_time
-            print('- done with', index, 'commits (', timePassed.seconds, ')s')
+            print('- done with', index, 'commits (', timePassed.seconds, ')s, wrote', writtenLines, 'lines')
 
-    repo.git.checkout(branch)
-    
-    result = csv.writer(open('gitLanguageHistory.csv', 'w'), delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    headers = ['commitSha', 'commitMessage', 'commitDate'] + allLanguages
-    result.writerow(headers)
-
-    for commit in commits:
-        row = []
-        for col in headers:
-            if col in commit:
-                row.append(commit[col])
-            else:
-                row.append(0)
-        result.writerow(row)
+    repo.git.checkout(branch)        
 
     timePassed = datetime.datetime.now() - begin_time
-    print('analysed', len(commits), 'commits (100%) in ', timePassed.seconds, 's')
-    print('saved results to gitLanguageHistory.csv')
+    print('analysed', commitCount, 'commits (100%) in ', timePassed.seconds, 's')
+    print('saved', writtenLines, 'lines to gitLanguageHistory.csv')
 if __name__ == "__main__":
     main()
