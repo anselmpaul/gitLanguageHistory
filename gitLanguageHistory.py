fdab8c63e57a868da99fbcd689d99ed39451c725
anselm <anselm.bunsen@sandstorm.de>
2021-06-01 13:05:55 +0200

    commented code for presentation

--- gitLanguageHistory.py
+++ gitLanguageHistory.py
@@ -1,14 +1,13 @@
 import git
-import os
 import subprocess
 import datetime
 import csv
-import math
 import argparse
 
 def main():
     begin_time = datetime.datetime.now()
-    parser = argparse.ArgumentParser(description='Create a history of the programming languages used in your git repo.')
+    parser = argparse.ArgumentParser(
+        description='Create a history of the programming languages used in your git repo.')
     parser.add_argument('--path', nargs='?', default='.',
                         help='path to your git repo, defaults to the current directory')
 
@@ -21,29 +20,54 @@ def main():
 
     print('analysing', commitCount, 'commits. This is going to take some time...')
 
-    result = csv.writer(open('gitLanguageHistory.csv', 'w'), delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    headers = ['commitSha', 'commitMessage', 'commitDate', 'language', 'percent']# + allLanguages
+    # setup the result CSV File
+    result = csv.writer(
+        open('gitLanguageHistory.csv', 'w'),
+        delimiter=';',
+        quotechar='|',
+        quoting=csv.QUOTE_MINIMAL
+    )
+    headers = ['commitSha', 'commitMessage', 'commitDate', 'language', 'percent']
     result.writerow(headers)
+    yearsAndWeeks = []
     
     for index, commit in enumerate(list(repo.iter_commits()), start=1):        
-        # need to filter out semis and linebreaks
+        # filter out semcolons and linebreaks for parsing
         commitMsg = commit.message.rstrip().replace(';', ',').replace('\n', ',')
         if "Merge branch" not in commitMsg:
-            repo.git.checkout(commit.hexsha)
-            linguistResult = subprocess.check_output('github-linguist', cwd=args.path.encode('utf-8')).decode("utf-8").split('\n')
-            for language in linguistResult:
-                row = [commit.hexsha, commitMsg, commit.committed_datetime.strftime("%d.%m.%Y"), "", ""]
-                percentAndLanguage = language.split()
-                if len(percentAndLanguage) > 1:
-                    row[3] = percentAndLanguage[1]
-                    row[4] = percentAndLanguage[0].replace('%', '')
-                    result.writerow(row)
-                    writtenLines = writtenLines + 1
+            # get commit date and check if there is already a commit for this week
+            date = datetime.datetime.fromtimestamp(commit.committed_date)
+            yearWeek = str(date.year) + '-' + str(date.isocalendar()[1])
+            if yearWeek not in yearsAndWeeks:
+                yearsAndWeeks.append(yearWeek)
+                # checkout the commit
+                repo.git.checkout(commit.hexsha)
+                # run linguist as subprocess
+                linguistResult = subprocess.check_output(
+                    'github-linguist',
+                    cwd=args.path.encode('utf-8')
+                    ).decode("utf-8").split('\n')
+                # parse the results
+                for language in linguistResult:
+                    row = [
+                        commit.hexsha,
+                        commitMsg,
+                        commit.committed_datetime.strftime("%d.%m.%Y"), 
+                        "", ""
+                    ]
+                    percentAndLanguage = language.split()
+                    if len(percentAndLanguage) > 1:
+                        row[3] = percentAndLanguage[1]
+                        row[4] = percentAndLanguage[0].replace('%', '')
+                        result.writerow(row)
+                        writtenLines = writtenLines + 1
 
         if (index > 0 and index % 10 == 0):
             timePassed = datetime.datetime.now() - begin_time
-            print('- done with', index, 'commits (', timePassed.seconds, ')s, wrote', writtenLines, 'lines')
+            print('- done with', index, 'commits (', timePassed.seconds,
+            ')s, wrote', writtenLines, 'lines')
 
+    # reset the repository
     repo.git.checkout(branch)        
 
     timePassed = datetime.datetime.now() - begin_time
